9c8111686cef36be2689e4f560364462cdeb792b
qwinsi <70425035+qwinsi@users.noreply.github.com>
2024-08-19 19:29:53 +0800

    migrate to tauri v2 and single instance according to https://v2.tauri.app/plugin/single-instance/

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{AppHandle, Manager};
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+/*
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+*/
+
+fn show_window(app: &AppHandle) {
+    let windows = app.webview_windows();
+
+    windows
+        .values()
+        .next()
+        .expect("Sorry, no window found")
+        .set_focus()
+        .expect("Can't Bring Window to Focus");
+}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = show_window(app);
+        }))
+        .plugin(tauri_plugin_shell::init())
+        // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
