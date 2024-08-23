c762045c088d2c659e88400da85b76a7f924b5ca
qwinsi <70425035+qwinsi@users.noreply.github.com>
2024-08-24 05:24:18 +0800

    update cheat-sheet.html

--- tools/make-cheat-sheet-html.js
+++ tools/make-cheat-sheet-html.js
@@ -3,9 +3,54 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import toml from 'toml';
 import fs from 'node:fs';
+import { tex2typst, symbolMap } from 'tex2typst';
+
+// TODO: https://personal.math.ubc.ca/~cautis/tools/latexmath.html
+//       https://typst.app/docs/reference/symbols/sym/
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const arrows = [
+    'leftarrow',
+    'gets',
+    'rightarrow',
+    'to',
+    'leftrightarrow',
+    'Leftarrow',
+    'Rightarrow',
+    'Leftrightarrow',
+    'mapsto',
+    'hookleftarrow',
+    'leftharpoonup',
+    'leftharpoondown',
+    'rightleftharpoons',
+    'longleftarrow',
+    'longrightarrow',
+    'longleftrightarrow',
+    'Longleftarrow',
+    'Longrightarrow',
+    'Longleftrightarrow',
+    'longmapsto',
+    'hookrightarrow',
+    'rightharpoonup',
+    'rightharpoondown',
+    'iff',
+    'implies',
+    'uparrow',
+    'downarrow',
+    'updownarrow',
+    'Uparrow',
+    'Downarrow',
+    'Updownarrow',
+    'nearrow',
+    'searrow',
+    'swarrow',
+    'nwarrow',
+    'leadsto',
+    'leftleftarrows',
+    'rightrightarrows',
+]
 
 function main() {
     const cheatSheetTomlFile = path.join(__dirname, '../src/template/cheat-sheet.toml');
@@ -20,6 +65,15 @@ function main() {
     for (const symbol of symbols_unsupported_by_katex) {
         delete data.math_symbols[symbol];
     }
+
+    data.arrow_symbols = {};
+    for(const arrow of arrows) {
+        if (!symbolMap.has(arrow)) {
+            throw new Error(`Symbol not found: ${arrow}`);
+        }
+        data.arrow_symbols[arrow] = symbolMap.get(arrow);
+    }
+
 
     const cheatSheetTemplateFile = path.join(__dirname, '../src/template/cheat-sheet.tp.html');
     const template = fs.readFileSync(cheatSheetTemplateFile, { encoding: 'utf-8' });
