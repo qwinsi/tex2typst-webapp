85c2640bec415a2f1601e454feec0d54e0f576a8
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-01-15 10:35:54 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,54 +1,45 @@
-// Adapted from https://blog.heroku.com/how-to-make-progressive-web-app
-
-const SUBDIRECTORY = "/tex2typst-webapp";
-
-self.addEventListener("install", function(event) {
-    event.waitUntil(preLoad());
-  });
-  
-  var preLoad = function(){
-    return caches.open("offline").then(function(cache) {
-      console.log("caching index and important routes");
-      return cache.addAll([SUBDIRECTORY + "/", SUBDIRECTORY + "/assets/", SUBDIRECTORY + "/offline.html"]);
+// node_modules/@qwinsi/utilities-js/sw.js
+function cleanResponse(response) {
+  const clonedResponse = response.clone();
+  const bodyPromise = "body" in clonedResponse ? Promise.resolve(clonedResponse.body) : clonedResponse.blob();
+  return bodyPromise.then((body) => {
+    return new Response(body, {
+      headers: clonedResponse.headers,
+      status: clonedResponse.status,
+      statusText: clonedResponse.statusText
     });
-  };
-  
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(checkResponse(event.request).catch(function() {
-      return returnFromCache(event.request);
-    }));
-    event.waitUntil(addToCache(event.request));
   });
-  
-  var checkResponse = function(request){
-    return new Promise(function(fulfill, reject) {
-      fetch(request).then(function(response){
-        if(response.status !== 404) {
-          fulfill(response);
-        } else {
-          reject();
+}
+function setup(precacheResources) {
+  const cacheName = "cache-v1";
+  self.addEventListener("install", (event) => {
+    console.info("[Service Worker] Caching resources...");
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+  });
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        if (cachedResponse.redirected === true && event.request.url.endsWith("/index.html")) {
+          return cleanResponse(cachedResponse);
         }
-      }, reject);
-    });
-  };
-  
-  var addToCache = function(request){
-    return caches.open("offline").then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log(response.url + " was cached");
-        return cache.put(request, response);
-      });
-    });
-  };
-  
-  var returnFromCache = function(request){
-    return caches.open("offline").then(function (cache) {
-      return cache.match(request).then(function (matching) {
-       if(!matching || matching.status == 404) {
-         return cache.match("offline.html");
-       } else {
-         return matching;
-       }
-      });
-    });
-  };
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }));
+  });
+}
+
+// src/sw.js
+var SUBDIRECTORY = "/tex2typst-webapp";
+var precacheResources = [
+  "/",
+  "/index.html",
+  "/main.js",
+  "/main.css",
+  "/manifest.json",
+  "/favicon.png",
+  "/impl-in-typst.html",
+  "/cheat-sheet.html"
+];
+var prefixedResources = precacheResources.map((resource) => SUBDIRECTORY + resource);
+setup(prefixedResources);
