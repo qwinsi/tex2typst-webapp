72f6db0ddd382eb96581a957cc5194e91efbb6c3
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-11-03 20:20:24 +0800

    bump tex2typst

--- tests/converter.test.js
+++ tests/converter.test.js
@@ -5,7 +5,7 @@ import { EXAMPLE_FORMULAS_LATEX  } from '../src/random.js';
 
 
 const EXPECTED_TYPST_FORMULAS = [
-    'upright(e) eq.def lim_(n -> infinity)(1 + 1/n)^n',
+    'upright(e) eq.def lim_(n -> infinity) (1 + 1/n)^n',
     'product_p 1/(1 - p^(-s)) = sum_(n = 1)^infinity 1/(n^s)',
     'a^(p - 1) equiv 1 mod p',
     String.raw`nabla times bold(H) &= bold(J) + (diff bold(D))/(diff t) \
@@ -19,18 +19,18 @@ nabla dot.op bold(D) &= rho`,
     'integral.triple_Omega op("div")(arrow(F)) dif V = integral.surf_(diff Omega) arrow(F) dot.op dif arrow(S)',
     'integral.double_Sigma op("curl")(arrow(F)) dot.op dif arrow(S) = integral.cont_(diff Sigma) arrow(F) times dif arrow(l)',
     'pi(x) ~ x/(log x)',
-    'i_D = mu_n C_"ox" W/L [(v_"GS" - V_t) v_"DS" - 1/2 v_"DS"^2 ]',
+    'i_D = mu_n C_"ox" W/L [(v_"GS" - V_t) v_"DS" - 1/2 v_"DS"^2]',
     String.raw`C &= N(d_1) S_t - N(d_2) K e^(-r t) \
 d_1 &= (ln (S_t)/K +(r + (sigma^2)/2) t)/(sigma sqrt(t)) \
 d_2 &= d_1 - sigma sqrt(t)`,
-    'f(z_0) = 1/(2 pi upright(i)) integral.cont_C  (f(z))/(z - z_0) dif z',
+    'f(z_0) = 1/(2 pi upright(i)) integral.cont_C (f(z))/(z - z_0) dif z',
     // TODO: There should no spaces after x_n and x_n^2
     String.raw`mat(delim: "|", 1, 1, ..., 1;
-x_1, x_2, ..., x_n ;
-x_1^2, x_2^2, ..., x_n^2 ;
+x_1, x_2, ..., x_n;
+x_1^2, x_2^2, ..., x_n^2;
 dots.v, dots.v, dots.down, dots.v;
 x_1^(n - 1), x_2^(n - 1), ..., x_n^(n - 1))
-= product_(1 <= i < j <= n)(x_j - x_i)`,
+= product_(1 <= i < j <= n) (x_j - x_i)`,
     String.raw`op("ReLU")(x) = cases(x & "if " x > 0,
 0 & "if " x <= 0)`,
     'op("Attention")(Q, K, V) = op("softmax")((Q K^T)/sqrt(d_k)) V',
